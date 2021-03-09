e8ab21b11c4e3fd21df92654750d080898fad2f2
Rob Bulmer <r95@hotmail.co.uk>
2021-03-09 21:37:30 +0000

    init sequelize on model index

--- bin/makemigration.js
+++ bin/makemigration.js
@@ -69,7 +69,7 @@ try {
 } catch (e) { }
 
 //console.log(path.join(migrationsDir, '_current.json'), JSON.parse(fs.readFileSync(path.join(migrationsDir, '_current.json') )))
-let sequelize = require(modelsDir);
+let sequelize = require(modelsDir).sequelize;
 
 let models = sequelize.models;
 
